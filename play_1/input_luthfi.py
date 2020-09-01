a8cfa4233ed1abf49ecb4645f4f002f60498f248
lanandra <luthfi.anandra@gmail.com>
2020-09-02 00:59:26 +0700

    rename dir play#1 to play_1

---
+++ play_1/input_luthfi.py
@@ -0,0 +1,21 @@
+# ask user to input text
+kata = str(input('Mohon masukkan kata disini : '))
+
+# condition about user input
+
+if kata == 'ulang':
+    print(kata)
+    print(kata)
+    print(kata)
+    print(kata)
+    print(kata)
+elif kata == 'lagi':
+    print(kata)
+    print(kata)
+    print(kata)
+    print(kata)
+    print(kata)
+    print(kata)
+    print(kata)
+else:
+    print('Saya sedang belajar programming')
